298eb7ff144f7bffe2dcdb056b61afacf4c3c2d2
Mark Hale <mj_hale@yahoo.com>
2016-05-22 14:59:55 +0100

    externalized rdfizer config

--- linked-data-sail/src/main/java/net/fortytwo/linkeddata/sail/config/LinkedDataSailConfig.java
+++ linked-data-sail/src/main/java/net/fortytwo/linkeddata/sail/config/LinkedDataSailConfig.java
@@ -6,11 +6,13 @@ import java.util.Map;
 import org.eclipse.rdf4j.model.IRI;
 import org.eclipse.rdf4j.model.Model;
 import org.eclipse.rdf4j.model.Resource;
+import org.eclipse.rdf4j.model.Value;
 import org.eclipse.rdf4j.model.ValueFactory;
 import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 import org.eclipse.rdf4j.model.util.Models;
 import org.eclipse.rdf4j.rio.ParserConfig;
 import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.rio.RDFParserRegistry;
 import org.eclipse.rdf4j.sail.config.AbstractDelegatingSailImplConfig;
 import org.eclipse.rdf4j.sail.config.SailConfigException;
 import org.eclipse.rdf4j.sail.config.SailImplConfig;
@@ -22,11 +24,18 @@ import net.fortytwo.linkeddata.rdfizers.VerbatimRdfizer;
 public class LinkedDataSailConfig extends AbstractDelegatingSailImplConfig {
 
 	private static final String NAMESPACE = "http://fortytwo.net/2012/02/linkeddata/config#";
+	private static final String MEDIA_TYPE_NS = "http://www.iana.org/assignments/media-types/";
 	private static final IRI MBEAN_NAME_PROPERTY;
+	private static final IRI RDFIZER_PROPERTY;
+	private static final IRI CONTENT_TYPE_PROPERTY;
+	private static final IRI RDF_FORMAT_PROPERTY;
 
 	static {
 		ValueFactory vf = SimpleValueFactory.getInstance();
 		MBEAN_NAME_PROPERTY = vf.createIRI(NAMESPACE+"mbeanName");
+		RDFIZER_PROPERTY = vf.createIRI(NAMESPACE+"rdfizer");
+		CONTENT_TYPE_PROPERTY = vf.createIRI(NAMESPACE+"contentType");
+		RDF_FORMAT_PROPERTY = vf.createIRI(NAMESPACE+"rdfFormat");
 	}
 
 	private final Map<MediaType, Rdfizer> rdfizers = new HashMap<>();
@@ -62,11 +71,19 @@ public class LinkedDataSailConfig extends AbstractDelegatingSailImplConfig {
 	public void parse(Model graph, Resource implNode) throws SailConfigException {
 		super.parse(graph, implNode);
 
-		mbeanName = Models.objectString(graph.filter(implNode, MBEAN_NAME_PROPERTY, null));
-
-		// hard-code config for now
+		RDFParserRegistry rdfRegistry = RDFParserRegistry.getInstance();
 		ParserConfig parserConfig = new ParserConfig();
-        // www.ipni.org
-        addRdfizer(MediaType.TEXT_XML, new VerbatimRdfizer(RDFFormat.RDFXML, parserConfig));
+		for(Value rdfizer : graph.filter(implNode, RDFIZER_PROPERTY, null).objects()) {
+			IRI contentTypeIri = Models.objectIRI(graph.filter((Resource) rdfizer, CONTENT_TYPE_PROPERTY, null));
+			IRI rdfFormatIri = Models.objectIRI(graph.filter((Resource) rdfizer, RDF_FORMAT_PROPERTY, null));
+			RDFFormat rdfFormat = rdfRegistry.getFileFormatForMIMEType(getMediaType(rdfFormatIri));
+	        addRdfizer(MediaType.valueOf(getMediaType(contentTypeIri)), new VerbatimRdfizer(rdfFormat, parserConfig));
+		}
+
+		mbeanName = Models.objectString(graph.filter(implNode, MBEAN_NAME_PROPERTY, null));
+	}
+
+	private String getMediaType(IRI iri) {
+		return iri.stringValue().substring(MEDIA_TYPE_NS.length());
 	}
 }
